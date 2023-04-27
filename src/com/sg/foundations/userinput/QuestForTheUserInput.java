80bdd36092fd3b23a82c14c8194a90e7f215bd20
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-04-27 02:50:16 +0100

    creating Scanner readLine for assigning a value to my string asking a question

--- src/com/sg/foundations/userinput/QuestForTheUserInput.java
+++ src/com/sg/foundations/userinput/QuestForTheUserInput.java
@@ -1,7 +1,18 @@
 package com.sg.foundations.userinput;
 
+import java.util.Scanner;
+
 public class QuestForTheUserInput {
     public static void main(String[] args){
+        Scanner inputReader = new Scanner(System.in);
+
+        String yourName;
+        String yourQuest;
+        double velocityOfSwallow;
+
+        // I can use the Scanner's readLine to assign value to my strings.
+        // because it's return type is string.
+        System.out.println("What is your name?? ");
 
     }
 }
