6a52e707a5cffa8eabfe8b9d2fb1296138c73678
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-04-27 03:10:22 +0100

    using Double.parseDouble to change the type of input the user makes to continue the messages

--- src/com/sg/foundations/userinput/QuestForTheUserInput.java
+++ src/com/sg/foundations/userinput/QuestForTheUserInput.java
@@ -8,7 +8,7 @@ public class QuestForTheUserInput {
 
         String yourName;
         String yourQuest;
-        double velocityOfSwallow;
+        double shoppingListPrice;
 
         // I can use the Scanner's readLine to assign value to my strings.
         // because it's return type is string.
@@ -18,5 +18,16 @@ public class QuestForTheUserInput {
         System.out.println("What is your quest?! ");
         yourQuest = inputReader.nextLine();
 
+        // When I get the double data type, I can use the Scanner's nextDouble method.
+
+
+        System.out.println("What was the cost of your last shopping list?!?! ");
+        // I can use the Double.parseDouble to convert the nextLine's String.
+        shoppingListPrice = Double.parseDouble(inputReader.nextLine());
+        System.out.println();
+        System.out.println("Why did you spend " + shoppingListPrice + " on that much shopping, " + yourName + "?");
+        System.out.println("That is an insane amount of money!");
+        System.out.println("Maybe skip being sad about money and instead go " + yourQuest + ".");
     }
+
 }
